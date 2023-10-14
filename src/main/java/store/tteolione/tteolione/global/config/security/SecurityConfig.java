5b8646e5291fa7f595ffc739f8eeb300fe725c78
Anwooseong <aws5624@naver.com>
2023-10-15 02:37:50 +0900

    Feat - 초기 설정: oauth2, jwt 도입 전 모든 권한 허용

---
+++ src/main/java/store/tteolione/tteolione/global/config/security/SecurityConfig.java
@@ -0,0 +1,37 @@
+package store.tteolione.tteolione.global.config.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .exceptionHandling()
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+}
