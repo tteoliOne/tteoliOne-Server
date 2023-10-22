7efec49e6424a7162098bfad9516578f1b77db1f
Anwooseong <aws5624@naver.com>
2023-10-22 16:46:30 +0900

    배포 테스트 컨트롤러

--- src/main/java/store/tteolione/tteolione/TestController.java
+++ src/main/java/store/tteolione/tteolione/TestController.java
@@ -1,5 +1,6 @@
 package store.tteolione.tteolione;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
+    @GetMapping
     public String test() {
         return "testSuccess";
     }
