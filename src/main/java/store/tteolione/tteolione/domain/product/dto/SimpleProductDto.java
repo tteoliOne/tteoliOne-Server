39829b6ae88d4cb0260645a5a768132f93c0f2bd
Anwooseong <aws5624@naver.com>
2023-10-28 01:31:52 +0900

    Fix: 상품 조회 dto title 추가 & liked필드명 수정

--- src/main/java/store/tteolione/tteolione/domain/product/dto/SimpleProductDto.java
+++ src/main/java/store/tteolione/tteolione/domain/product/dto/SimpleProductDto.java
@@ -7,19 +7,21 @@ import lombok.*;
 public class SimpleProductDto {
     private long productId;
     private String imageUrl;
+    private String title;
     private int unitPrice;
     private double walkingDistance;
     private int walkingTime;
     private int totalLikes;
-    private boolean isLiked;
+    private boolean liked;
 
-    public SimpleProductDto(long productId, String imageUrl, int unitPrice, double walkingDistance, int walkingTime, int totalLikes, boolean isLiked) {
+    public SimpleProductDto(long productId, String imageUrl, String title, int unitPrice, double walkingDistance, int walkingTime, int totalLikes, boolean liked) {
         this.productId = productId;
         this.imageUrl = imageUrl;
+        this.title = title;
         this.unitPrice = unitPrice;
         this.walkingDistance = walkingDistance;
         this.walkingTime = walkingTime;
         this.totalLikes = totalLikes;
-        this.isLiked = isLiked;
+        this.liked = liked;
     }
 }
