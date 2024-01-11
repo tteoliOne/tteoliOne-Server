b66f933380dabb3453cf42755ac25ddafc4ed626
Anwooseong <aws5624@naver.com>
2024-01-12 00:28:06 +0900

    Fix: 상품 간단조회, 목록조회 -> 상품 판매 상태 추가

--- src/main/java/store/tteolione/tteolione/domain/product/repository/ProductRepositoryCustomImpl.java
+++ src/main/java/store/tteolione/tteolione/domain/product/repository/ProductRepositoryCustomImpl.java
@@ -59,6 +59,7 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
                         calculateWalkingDistance(longitude, latitude).as("walkingDistance"),
                         calculateWalkingTime(longitude, latitude).as("walkingTime"),
                         product.likeCount.as("totalLikes"),
+                        product.soldStatus.as("soldStatus"),
                         likes.likeId,
                         likes.likeStatus.as("liked")
                 ))
@@ -97,6 +98,7 @@ public class ProductRepositoryCustomImpl implements ProductRepositoryCustom {
                         calculateWalkingDistance(longitude, latitude).as("walkingDistance"),
                         calculateWalkingTime(longitude, latitude).as("walkingTime"),
                         product.likeCount.as("totalLikes"),
+                        product.soldStatus.as("soldStatus"),
                         likes.likeId,
                         likes.likeStatus.as("liked")
                 ))
