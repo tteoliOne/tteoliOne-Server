418fac78277bb3ebadef50c96bd57830a1b2f13d
Anwooseong <aws5624@naver.com>
2024-01-12 01:55:08 +0900

    Fix: NotBlack -> NotNull 수정

--- src/main/java/store/tteolione/tteolione/domain/product/dto/PostProductRequest.java
+++ src/main/java/store/tteolione/tteolione/domain/product/dto/PostProductRequest.java
@@ -7,6 +7,7 @@ import store.tteolione.tteolione.domain.product.constants.ProductConstants;
 import store.tteolione.tteolione.domain.product.entity.Product;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 
@@ -15,34 +16,34 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class PostProductRequest {
 
-    @NotBlank(message = "카테고리 Id를 입력해주세요.")
+    @NotNull(message = "카테고리 Id를 입력해주세요.")
     private Long categoryId;
 
-    @NotBlank(message = "상품 제목을 입력해주세요.")
+    @NotNull(message = "상품 제목을 입력해주세요.")
     private String title;
 
-    @NotBlank(message = "구입 가격을 입력해주세요.")
+    @NotNull(message = "구입 가격을 입력해주세요.")
     private int buyPrice;
 
-    @NotBlank(message = "구입 수량을 입력해주세요.")
+    @NotNull(message = "구입 수량을 입력해주세요.")
     private int buyCount;
 
-    @NotBlank(message = "공유 가격을 입력해주세요.")
+    @NotNull(message = "공유 가격을 입력해주세요.")
     private int sharePrice;
 
-    @NotBlank(message = "공유 수량을 입력해주세요.")
+    @NotNull(message = "공유 수량을 입력해주세요.")
     private int shareCount;
 
-    @NotBlank(message = "구매 일자를 입력해주세요.")
+    @NotNull(message = "구매 일자를 입력해주세요.")
     private LocalDateTime buyDate;
 
-    @NotBlank(message = "상세 설명을 입력해주세요.")
+    @NotNull(message = "상세 설명을 입력해주세요.")
     private String description;
 
-    @NotBlank(message = "경도를 입력해주세요.")
+    @NotNull(message = "경도를 입력해주세요.")
     private double longitude;
 
-    @NotBlank(message = "위도를 입력해주세요.")
+    @NotNull(message = "위도를 입력해주세요.")
     private double latitude;
 
 
