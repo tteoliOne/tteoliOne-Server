24f064a4e8932a1c26b74b1c8306d11854b645dd
Anwooseong <aws5624@naver.com>
2025-01-18 17:31:27 +0900

    fix: 접근 거부시 상태 코드 변경

--- src/main/java/store/tteolione/tteolione/global/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/store/tteolione/tteolione/global/jwt/JwtAuthenticationEntryPoint.java
@@ -18,19 +18,30 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         String exception = (String)request.getAttribute("exception");
-        if(exception == null) setResponse(response, ExceptionCode.ACCESS_DENIED);
+        if(exception == null) setAccessDenied(response, ExceptionCode.ACCESS_DENIED);
         //잘못된 타입의 토큰인 경우
         else if(exception.equals(ExceptionCode.WRONG_TYPE_TOKEN.getCode())) setResponse(response, ExceptionCode.WRONG_TYPE_TOKEN);
         //토큰 만료된 경우
         else if(exception.equals(ExceptionCode.EXPIRED_TOKEN.getCode())) setResponse(response, ExceptionCode.EXPIRED_TOKEN);
         //지원되지 않는 토큰인 경우
         else if(exception.equals(ExceptionCode.UNSUPPORTED_TOKEN.getCode())) setResponse(response, ExceptionCode.UNSUPPORTED_TOKEN);
-        else setResponse(response, ExceptionCode.ACCESS_DENIED);
+        else setAccessDenied(response, ExceptionCode.ACCESS_DENIED);
     }
 
     private void setResponse(HttpServletResponse response, ExceptionCode exceptionCode) throws IOException {
         response.setContentType("application/json;charset=UTF-8");
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        JSONObject responseJson = new JSONObject();
+        responseJson.put("messages", Arrays.asList(exceptionCode.getMessage()));
+        responseJson.put("code", exceptionCode.getCode());
+
+        response.getWriter().print(responseJson);
+    }
+
+    private void setAccessDenied(HttpServletResponse response, ExceptionCode exceptionCode) throws IOException {
+        response.setContentType("application/json;charset=UTF-8");
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
 
         JSONObject responseJson = new JSONObject();
         responseJson.put("messages", Arrays.asList(exceptionCode.getMessage()));
