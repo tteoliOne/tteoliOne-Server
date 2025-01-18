58cdd2ad1743340efdfb7531f14c42ab13a21749
Anwooseong <aws5624@naver.com>
2025-01-18 17:32:51 +0900

    fix: 접근 거부시 상태 코드 변경

--- src/main/java/store/tteolione/tteolione/global/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/store/tteolione/tteolione/global/jwt/JwtAuthenticationEntryPoint.java
@@ -19,7 +19,7 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
         String exception = (String)request.getAttribute("exception");
         if(exception == null) setAccessDenied(response, ExceptionCode.ACCESS_DENIED);
-        //잘못된 타입의 토큰인 경우
+        //잘못된 타입의 토큰인 경우경
         else if(exception.equals(ExceptionCode.WRONG_TYPE_TOKEN.getCode())) setResponse(response, ExceptionCode.WRONG_TYPE_TOKEN);
         //토큰 만료된 경우
         else if(exception.equals(ExceptionCode.EXPIRED_TOKEN.getCode())) setResponse(response, ExceptionCode.EXPIRED_TOKEN);
